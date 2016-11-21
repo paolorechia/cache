270b9439e8b0d1cfccd77f30914a2a3607b8b849
Paolo Rechia <pasr13@inf.ufpr.br>
2016-11-21 17:28:47 -0200

    extrator de taxa de faltas simples

--- extractor.sh
+++ extractor.sh
@@ -2,11 +2,18 @@
 
 
 function extractFetches {
-        grep Fetches | grep -v \(
-    }
-
-function extractMisses {
-    grep Misses 
+    grep Fetches | grep -v \( | tr -s ' ' | cut -f3 | cut -f2 -d' '
 }
 
-cat $1 | extractFetches ; cat $1 | extractMisses
+function extractMisses {
+    grep Misses | tr -s ' ' | cut -f3 | cut -f2 -d' '
+}
+
+fetches=$(cat $1 | extractFetches)
+misses=$(cat $1 | extractMisses)
+echo Fetches = $fetches
+echo Misses = $misses
+taxa_faltas=$(echo "scale=2; $fetches/$misses"  | bc -l)
+echo $taxa_faltas
+
+echo Taxa de faltas = $taxa_faltas%
