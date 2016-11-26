451f768fb5b6a64af07e5b10419a9a51c8aa4c27
Paolo Andreas Stall Rechia <pasr13@inf.ufpr.br>
2016-11-26 19:58:29 -0200

    adicionando um if no make-plot pra facilitar minha vida

--- make-plot.sh
+++ make-plot.sh
@@ -1,12 +1,19 @@
 #/bin/bash
 
+
+if [ $computer="HOME" ]; then
+    visualizer=eom
+    else
+    visualizer=eog
+fi
+
 if [ -n $arg1 ] && [ -a $arg1 ]; then
 	echo "./strip.sh $1 > stripped_$1"
 	./strip.sh $1 > stripped_$1 
 	echo "./build_dat.sh $1 > trab162/plots/assoc-bl.dat"
 	./build_dat.sh $1 > trab162/plots/assoc-bl.dat
-	echo "( cd trab162/plots ; ./dat-assoc-bl.gp > $1 ; eog $1 )"
-	( cd trab162/plots ; ./dat-assoc-bl.gp > $1.png ; eog $1.png )
+	echo "( cd trab162/plots ; ./dat-assoc-bl.gp > $1 ; $visualizer $1 )"
+	( cd trab162/plots ; ./dat-assoc-bl.gp > $1.png ; $visualizer $1.png )
 else
     echo "Passe um argumento, e.g. 'area4k'"
 fi
